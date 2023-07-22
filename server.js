26d09f83bfae8f619a52b467b1019c1146b9834b
clintjeff2 <clientjeff2@gmail.com>
2023-07-22 11:05:52 +0100

    Initial commit for YouTube Clone starter project

---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require('./App');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+//READING 
+dotenv.config({path: './config.env'});
+const url = process.env.DATABASE_STRING.replace('<password>', process.env.DATABASE_PASSWORD);
+
+//CONNECTING TO DATABASE
+mongoose.connect(url).then( () => console.log('Connected to DB')).catch(err => console.log(err));
+
+//SERVER SETUP
+app.listen(process.env.PORT, process.env.LOCALHOST, () => {
+    console.log('Server running on port ', process.env.PORT);
+});
