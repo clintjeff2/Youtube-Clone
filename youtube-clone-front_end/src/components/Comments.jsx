13f7b3cf3359d1f9c56eeb969c485366daba3ce1
clintjeff2 <clientjeff2@gmail.com>
2023-08-10 16:14:29 +0100

    Created the Comments component for the videos

---
+++ youtube-clone-front_end/src/components/Comments.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './comments.module.css';
+
+function Comments() {
+	return (
+		<div className={styles.comments}>
+			<h3>2760 Comments</h3>
+		</div>
+	);
+}
+
+export default Comments;
